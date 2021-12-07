41597af6b1675e23274a329b48ed81fb2ac7d36a
Emilse <emilsearias@gmail.com>
2021-12-07 11:37:51 -0300

    agregamos midlewares en index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
+//instanciar express
 const app = express();
+//crear un puerto
+app.set('port', process.env.PORT || 4000);
 
-const port = 4000;
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('port'));
+})
+//configuraciones extras midlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.listen(port, ()=>{
-    console.log('Estoy en el puerto '+port);
+
+//crear rutas
+app.get('/', (req, res)=>{
+    res.send('hola esto es una prueba desde el backend')
+})
+app.delete('/borrarproducto', (req, res)=>{
+    res.send('alguien quiere borrar')
 })
 
-console.log('hola mundo')

22ee0989c7f2833c53335f8310f7a5e91ca6c970
Emilse <emilsearias@gmail.com>
2021-12-09 10:43:11 -0300

    se agrego el index.html a la carpeta public

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
 //instanciar express
 const app = express();
@@ -9,13 +10,15 @@ app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+ app.get('port'));
+    console.log(path.join(__dirname,'../public'))
 })
 //configuraciones extras midlewares
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+//configurar el index.html
+app.use(express.static(path.join(__dirname,'../public')));
 
 //crear rutas
 app.get('/', (req, res)=>{
